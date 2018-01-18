401515a02b89e48876d4ee6e29ffaa33274714ce
samimis <samimi.sam@gmail.com>
2018-01-17 16:14:28 -0800

    Added Segues

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Work on 1/17/18.
+//  Copyright © 2018 sam samimi. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
